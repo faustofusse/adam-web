247388430cd64aa7c152fcc93116a6d4ceb26db0
Fausto Fusse <faustofusse@Faustos-MacBook-Pro.local>
2019-11-18 22:58:12 -0300

    Intento agregar CORS

--- app.js
+++ app.js
@@ -25,6 +25,11 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
 // middlewares
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use(logger('dev'));
 app.use(methodOverride('_method'));
 // for parsing application/json
